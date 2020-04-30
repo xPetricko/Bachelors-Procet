576c2f36d1ebcb0af56570b75a4ab4d8e99663a2
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2020-04-30 17:23:55 +0200

    Repaired train.py

--- bp/train.py
+++ bp/train.py
@@ -11,24 +11,30 @@ from model import *
 
 import matplotlib.pyplot as plt
 
-def save_args(TRAIN_NO,args):
-    args_name = ["alpha","ganna","action_repeat","img_stack","seed","render","log_iterval"]
+
+def save_args(TRAIN_NO, args):
+    args_name = ["alpha", "ganna", "action_repeat",
+                 "img_stack", "seed", "render", "log_iterval"]
     with open("data/args/"+str(TRAIN_NO)+'args.txt', 'w') as f:
-                f.write(str(args))
+        f.write(str(args))
 
-
-
-
-parser = argparse.ArgumentParser(description='Bachelors project Andrej Petricko PPO Reinforcement Learning')
-parser.add_argument('--alpha', type=float, default=1e-3, metavar='G', help='discount factor (default: 0.0001)')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G', help='discount factor (default: 0.99)')
-parser.add_argument('--action-repeat', type=int, default=8, metavar='N', help='repeat action in N frames (default: 8)')
-parser.add_argument('--img-stack', type=int, default=4, metavar='N', help='stack N image in a state (default: 4)')
-parser.add_argument('--seed', type=int, default=0, metavar='N', help='random seed (default: 0)')
-parser.add_argument('--render', action='store_true', help='render the environment')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='interval between training status logs (default: 10)')
+parser = argparse.ArgumentParser(
+    description='Bachelors project Andrej Petricko PPO Reinforcement Learning')
+parser.add_argument('--alpha', type=float, default=1e-3,
+                    metavar='G', help='discount factor (default: 0.0001)')
+parser.add_argument('--gamma', type=float, default=0.99,
+                    metavar='G', help='discount factor (default: 0.99)')
+parser.add_argument('--action-repeat', type=int, default=8,
+                    metavar='N', help='repeat action in N frames (default: 8)')
+parser.add_argument('--img-stack', type=int, default=4,
+                    metavar='N', help='stack N image in a state (default: 4)')
+parser.add_argument('--seed', type=int, default=0,
+                    metavar='N', help='random seed (default: 0)')
+parser.add_argument('--render', action='store_true',
+                    help='render the environment')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='interval between training status logs (default: 10)')
 args = parser.parse_args()
-
 
 
 NUM_EPISODES = 100000
@@ -43,22 +49,24 @@ if torch.cuda.is_available():
 
 
 if __name__ == "__main__":
-    save_args(TRAIN_NO,args)
+    save_args(TRAIN_NO, args)
 
     agent = Agent(alpha=args.alpha, gamma=args.gamma, img_stack=args.img_stack)
-    env = Env(seed=args.seed, action_repeat=args.action_repeat, img_stack=args.img_stack)
-    training_records = []
+    env = Env(seed=args.seed, action_repeat=args.action_repeat,
+              img_stack=args.img_stack)
     running_score = 0
     state = env.reset()
     max_running_score = 100
-    score_history =[]
+    score_history = []
+    running_score_history = []
     for episode in range(NUM_EPISODES):
         score = 0
         state = env.reset()
-        
+
         for t in range(MAX_STEPS):
             action, a_logp = agent.select_action(state)
-            state_, reward, done, die = env.step(action * np.array([2., 1., 1.]) + np.array([-1., 0., 0.]))
+            state_, reward, done, die = env.step(
+                action * np.array([2., 1., 1.]) + np.array([-1., 0., 0.]))
             if args.render:
                 env.render()
             if agent.store((state, action, a_logp, reward, state_)):
@@ -68,13 +76,14 @@ if __name__ == "__main__":
             state = state_
             if done or die:
                 break
-            
+
         running_score = running_score * 0.99 + score * 0.01
+        running_score_history.append(running_score)
         score_history.append(score)
-        
 
         if episode % args.log_interval == 0:
-            print('Ep {}\tLast score: {:.2f}\tMoving average score: {:.2f}'.format(episode, score, running_score))
+            print('Ep {}\tLast score: {:.2f}\tMoving average score: {:.2f}'.format(
+                episode, score, running_score))
             name = str(TRAIN_NO) + "_automatic_save_params"
             agent.save_param(name=name)
             with open("data/score_history/"+str(TRAIN_NO)+'_score_history.txt', 'w') as f:
@@ -82,15 +91,16 @@ if __name__ == "__main__":
                 f.truncate
                 f.writelines("%f\n" % score for score in score_history)
             with open("data/score_history/"+str(TRAIN_NO)+'_runnig_history.txt', 'w') as f:
-                f.writelines("%f\n" % running_score)
-
-
-        if running_score>max_running_score:
-            max_running_score+=100
-            name = str(TRAIN_NO)+"_"+"score_"+str(running_score)+"episode_"+str(episode)+"_progres_save_params"
+                f.seek(0)
+                f.truncate
+                f.writelines("%f\n" % score for score in running_score)
+        if running_score > max_running_score:
+            max_running_score += 100
+            name = str(TRAIN_NO)+"_"+"score_"+str(running_score) + \
+                "episode_"+str(episode)+"_progres_save_params"
             agent.save_param(name=name)
-            
 
         if running_score > env.reward_threshold:
-            print("Solved! Running reward is now {} and the last episode runs to {}!".format(running_score, score))
+            print("Solved! Running reward is now {} and the last episode runs to {}!".format(
+                running_score, score))
             break
