1ecb45e2c24b97fb9ff22ae4d3a1930268482751
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2020-05-04 18:21:18 +0200

    01

--- bp/env.py
+++ bp/env.py
@@ -29,15 +29,16 @@ class Env():
         total_reward = 0
         for i in range(self.action_repeat):
             img_rgb, reward, die, _ = self.env.step(action)
+            done = die
             # don't penalize "done state"
-            if die:
-                reward += 100
+#            if die:
+#                reward += 100
             # green penalty
-            if np.mean(img_rgb[:, :, 1]) > 185.0:
-                reward -= 0.05
+#            if np.mean(img_rgb[:, :, 1]) > 185.0:
+#                reward -= 0.05
             total_reward += reward
             # if no reward recently, end the episode
-            done = True if self.av_r(reward) <= -0.1 else False
+#            done = True if self.av_r(reward) <= -0.1 else False
             if done or die:
                 break
 
