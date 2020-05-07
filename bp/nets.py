f4ba01199faebf7e16da879d208c90c4ba1534b3
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2020-05-07 09:02:42 +0200

    02

--- bp/nets.py
+++ bp/nets.py
@@ -6,13 +6,11 @@ from torch.distributions import Beta
 from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 
 
-
 class Net(nn.Module):
-    def __init__(self,gamma, img_stack,alpha=1e-3):
+    def __init__(self, gamma, img_stack, alpha=1e-3):
         super(Net, self).__init__()
         self.img_stack = img_stack
         self.gamma = gamma
-
 
         self.cnn_base = nn.Sequential(  # input shape (4, 96, 96)
             nn.Conv2d(self.img_stack, 8, kernel_size=4, stride=2),
@@ -28,7 +26,7 @@ class Net(nn.Module):
             nn.Conv2d(128, 256, kernel_size=3, stride=1),  # (128, 3, 3)
             nn.ReLU(),  # activation
         )  # output shape (256, 1, 1)
-        self.v = nn.Sequential(nn.Linear(256, 100), nn.ReLU())
+        self.v = nn.Sequential(nn.Linear(256, 1), nn.ReLU())
         self.fc = nn.Sequential(nn.Linear(256, 100), nn.ReLU())
         self.alpha_head = nn.Sequential(nn.Linear(100, 3), nn.Softplus())
         self.beta_head = nn.Sequential(nn.Linear(100, 3), nn.Softplus())
@@ -38,7 +36,8 @@ class Net(nn.Module):
     @staticmethod
     def _weights_init(m):
         if isinstance(m, nn.Conv2d):
-            nn.init.xavier_uniform_(m.weight, gain=nn.init.calculate_gain('relu'))
+            nn.init.xavier_uniform_(
+                m.weight, gain=nn.init.calculate_gain('relu'))
             nn.init.constant_(m.bias, 0.1)
 
     def forward(self, x):
@@ -52,24 +51,24 @@ class Net(nn.Module):
         return (alpha, beta), v
 
 
-
 class NetBN(nn.Module):
-    def __init__(self,gamma, img_stack,alpha=1e-3):
+    def __init__(self, gamma, img_stack, alpha=1e-3):
         super(NetBN, self).__init__()
         self.img_stack = img_stack
         self.gamma = gamma
-
 
         self.cnn_base = nn.Sequential(  # input shape (4, 96, 96)
             nn.Conv2d(self.img_stack, 8, kernel_size=4, stride=2),
             nn.BatchNorm2d(8),
             nn.ReLU(),  # activation
             nn.Conv2d(8, 16, kernel_size=3, stride=2),  # (8, 47, 47)
+            nn.BatchNorm2d(16),
             nn.ReLU(),  # activation
             nn.Conv2d(16, 32, kernel_size=3, stride=2),  # (16, 23, 23)
             nn.BatchNorm2d(32),
             nn.ReLU(),  # activation
             nn.Conv2d(32, 64, kernel_size=3, stride=2),  # (32, 11, 11)
+            nn.BatchNorm2d(64),
             nn.ReLU(),  # activation
             nn.Conv2d(64, 128, kernel_size=3, stride=1),  # (64, 5, 5)
             nn.BatchNorm2d(128),
@@ -77,7 +76,7 @@ class NetBN(nn.Module):
             nn.Conv2d(128, 256, kernel_size=3, stride=1),  # (128, 3, 3)
             nn.ReLU(),  # activation
         )  # output shape (256, 1, 1)
-        self.v = nn.Sequential(nn.Linear(256, 100), nn.ReLU())
+        self.v = nn.Sequential(nn.Linear(256, 1), nn.ReLU())
         self.fc = nn.Sequential(nn.Linear(256, 100), nn.ReLU())
         self.alpha_head = nn.Sequential(nn.Linear(100, 3), nn.Softplus())
         self.beta_head = nn.Sequential(nn.Linear(100, 3), nn.Softplus())
@@ -87,7 +86,8 @@ class NetBN(nn.Module):
     @staticmethod
     def _weights_init(m):
         if isinstance(m, nn.Conv2d):
-            nn.init.xavier_uniform_(m.weight, gain=nn.init.calculate_gain('relu'))
+            nn.init.xavier_uniform_(
+                m.weight, gain=nn.init.calculate_gain('relu'))
             nn.init.constant_(m.bias, 0.1)
 
     def forward(self, x):
@@ -99,4 +99,3 @@ class NetBN(nn.Module):
         beta = self.beta_head(x) + 1
 
         return (alpha, beta), v
-
