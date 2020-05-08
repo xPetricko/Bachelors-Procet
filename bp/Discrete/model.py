806ffac9897a05562527a8afe1283c60473320ad
Andrej Petričko <32256397+xPetricko@users.noreply.github.com>
2020-05-08 13:27:40 +0200

    Add discrete env

---
+++ bp/Discrete/model.py
@@ -0,0 +1,101 @@
+import numpy as np
+
+import torch as T
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Beta
+from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
+
+from nets import Net
+
+
+class Agent():
+    max_grad_norm = 0.5
+    clip_param = 0.1  # epsilon in clipped loss
+    ppo_epoch = 10
+    buffer_capacity, batch_size = 2000, 128
+
+    def __init__(self, alpha, gamma, img_stack, nn_type):
+        self.alpha = alpha
+        self.gamma = gamma
+        self.img_stack = img_stack
+
+        self.transition = np.dtype([('s', np.float64, (self.img_stack, 96, 96)), ('a', np.int, (1,)), ('a_logp', np.float64),
+                                    ('r', np.float64), ('s_n', np.float64, (self.img_stack, 96, 96))])
+        self.training_step = 0
+        self.device = T.device("cuda:0" if T.cuda.is_available() else "cpu")
+        if nn_type == 0:
+            self.net = Net(alpha=self.alpha, gamma=self.gamma,
+                           img_stack=self.img_stack).double().to(self.device)
+        else:
+            self.net = NetBN(alpha=self.alpha, gamma=self.gamma,
+                             img_stack=self.img_stack).double().to(self.device)
+        self.buffer = np.empty(self.buffer_capacity, dtype=self.transition)
+        self.counter = 0
+
+    def select_action(self, state):
+        state = T.from_numpy(state).double().to(self.device).unsqueeze(0)
+        with T.no_grad():
+            action_probs = self.net(state)[0]
+        action_probs = T.distributions.Categorical(action_probs)
+        action = action_probs.sample()
+        a_logp = action_probs.log_prob(action)
+
+        return action.cpu().numpy(), a_logp
+
+    def save_param(self, name):
+        T.save(self.net.state_dict(), 'data/param/'+name+'params.pkl')
+
+    def store(self, transition):
+        self.buffer[self.counter] = transition
+        self.counter += 1
+        if self.counter == self.buffer_capacity:
+            self.counter = 0
+            return True
+        else:
+            return False
+
+    def load_param(name):
+        self.net.load_state_dict(torch.load('data/param/"'+name+'.pkl'))
+
+    def update(self):
+        self.training_step += 1
+
+        s = T.tensor(self.buffer['s'], dtype=T.double).to(self.device)
+        a = T.tensor(self.buffer['a'], dtype=T.int).to(self.device)
+        r = T.tensor(self.buffer['r'], dtype=T.double).to(
+            self.device).view(-1, 1)
+        s_n = T.tensor(self.buffer['s_n'], dtype=T.double).to(self.device)
+
+        old_a_logp = T.tensor(self.buffer['a_logp'], dtype=T.double).to(
+            self.device).view(-1, 1)
+
+        with T.no_grad():
+            target_v = r + self.gamma * self.net(s_n)[1]
+            adv = target_v - self.net(s)[1]
+            # adv = (adv - adv.mean()) / (adv.std() + 1e-8) #optional
+
+        for _ in range(self.ppo_epoch):
+            for index in BatchSampler(SubsetRandomSampler(range(self.buffer_capacity)), self.batch_size, False):
+
+                action = self.net(s[index])[0]
+
+                a_logp = action.log_prob(a[index]).sum(dim=1, keepdim=True)
+
+                ratio = T.exp(a_logp - old_a_logp[index])
+
+                surr1 = ratio * adv[index]
+                surr2 = T.clamp(ratio, 1.0 - self.clip_param,
+                                1.0 + self.clip_param) * adv[index]
+                action_loss = -T.min(surr1, surr2).mean()
+                value_loss = F.smooth_l1_loss(
+                    self.net(s[index])[1], target_v[index])
+                loss = action_loss + 2. * value_loss
+
+                self.net.optimizer.zero_grad()
+                loss.backward()
+                # nn.utils.clip_grad_norm_(self.net.parameters(), self.max_grad_norm) #optional
+                self.net.optimizer.step()
+
+    
---
+++ bp/Discrete/train.py
@@ -0,0 +1,116 @@
+import argparse
+
+import numpy as np
+
+import torch
+
+
+from env import Env
+from model import *
+
+
+import matplotlib.pyplot as plt
+
+
+def save_args(TRAIN_NO, args):
+    args_name = ["alpha", "ganna", "action_repeat",
+                 "img_stack", "seed", "render", "log_iterval"]
+    with open("data/args/"+str(TRAIN_NO)+'args.txt', 'w') as f:
+        f.write(str(args))
+
+
+parser = argparse.ArgumentParser(
+    description='Bachelors project Andrej Petricko PPO Reinforcement Learning')
+parser.add_argument('--alpha', type=float, default=1e-3,
+                    metavar='G', help='discount factor (default: 0.0001)')
+parser.add_argument('--gamma', type=float, default=0.99,
+                    metavar='G', help='discount factor (default: 0.99)')
+parser.add_argument('--action-repeat', type=int, default=8,
+                    metavar='N', help='repeat action in N frames (default: 8)')
+parser.add_argument('--img-stack', type=int, default=4,
+                    metavar='N', help='stack N image in a state (default: 4)')
+parser.add_argument('--seed', type=int, default=0,
+                    metavar='N', help='random seed (default: 0)')
+parser.add_argument('--render', action='store_true',
+                    help='render the environment')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='interval between training status logs (default: 10)')
+parser.add_argument('--nn-type', type=int, default=0, metavar='N',
+                    help='nn-type')
+args = parser.parse_args()
+
+
+NUM_EPISODES = 100000
+MAX_STEPS = 2000
+
+TRAIN_NO = 10
+
+
+torch.manual_seed(args.seed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed(args.seed)
+
+
+if __name__ == "__main__":
+    save_args(TRAIN_NO, args)
+
+    agent = Agent(alpha=args.alpha, gamma=args.gamma, img_stack=args.img_stack, nn_type=args.nn_type)
+    env = Env(seed=args.seed, action_repeat=args.action_repeat,
+              img_stack=args.img_stack)
+    running_score = 0
+    state = env.reset()
+    max_running_score = 100
+    score_history = []
+    running_score_history = []
+    for episode in range(NUM_EPISODES):
+        score = 0
+        state = env.reset()
+
+        for t in range(MAX_STEPS):
+            action, a_logp = agent.select_action(state)
+            new_state, reward, done, die = env.step(action)
+            if args.render:
+                env.render()
+            if agent.store((state, action, a_logp, reward, new_state)):
+                print('updating')
+                agent.update()
+            score += reward
+            state = new_state
+            if done or die:
+                break
+
+        running_score = running_score * 0.99 + score * 0.01
+        running_score_history.append(running_score)
+        score_history.append(score)
+
+        if episode % args.log_interval == 0:
+            print('Ep {}\tLast score: {:.2f}\tMoving average score: {:.2f}'.format(
+                episode, score, running_score))
+            name = str(TRAIN_NO) + "_automatic_save_params"
+            agent.save_param(name=name)
+            with open("data/score_history/"+str(TRAIN_NO)+'_score_history.txt', 'w') as f:
+                f.seek(0)
+                f.truncate
+                f.writelines("%f\n" % score for score in score_history)
+            with open("data/score_history/"+str(TRAIN_NO)+'_runnig_history.txt', 'w') as f:
+                f.seek(0)
+                f.truncate
+                f.writelines("%f\n" % score for score in running_score_history)
+        if running_score > max_running_score:
+            max_running_score += 10
+            name = str(TRAIN_NO)+"_"+"score_"+str(running_score) + \
+                "episode_"+str(episode)+"_progres_save_params"
+            agent.save_param(name=name)
+
+        if running_score > env.reward_threshold+50:
+            print("Solved! Running reward is now {} and the last episode runs to {}!".format(
+                running_score, score))
+            with open("data/score_history/"+str(TRAIN_NO)+'_score_history.txt', 'w') as f:
+                f.seek(0)
+                f.truncate
+                f.writelines("%f\n" % score for score in score_history)
+            with open("data/score_history/"+str(TRAIN_NO)+'_runnig_history.txt', 'w') as f:
+                f.seek(0)
+                f.truncate
+                f.writelines("%f\n" % score for score in running_score_history)
+            break
